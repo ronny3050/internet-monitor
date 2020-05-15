7fdf5d14e7715482d97f00503094db406986eab1
Brian Hepler <brian.hepler@verizon.net>
2020-05-14 23:39:35 -0400

    Refactored to JustGage 1.3.5 added gitignore

--- internet-monitor.js
+++ internet-monitor.js
@@ -23,8 +23,8 @@ Module.register("internet-monitor",{
 
 	start : function(){
 		console.log("Internet-monitor module started!");
-
-		this.sendSocketNotification("Start",this.config);
+		
+		// this.sendSocketNotification("Start",this.config);
 		// Schedule update timer.
 		var self = this;
 		if(this.config.updateInterval != 0)
@@ -33,108 +33,41 @@ Module.register("internet-monitor",{
 				self.updating= true;
 				self.sendSocketNotification("Start",self.config);
 			}, this.config.updateInterval);
+		} else {
+			setTimeout(function() {
+				self.updating= true;
+				self.sendSocketNotification("Start",self.config);
+			}, 2000);
 		}
+		
 	},
 
-	getScripts: function(){
-		return [this.file("justgage-1.2.2/justgage.js"),this.file("justgage-1.2.2/raphael-2.1.4.min.js"),this.file("jquery.js")]
+	getScripts: function() {
+		return [this.file("justgage-1.2.2/raphael.2.2.8.min.js"),
+			this.file("justgage-1.2.2/justgage.min.js"),
+			this.file("jquery.js")];
 	},
 
-	getStyles: function(){
+	getStyles: function() {
 		return ["internet-monitor.css"]
-	},
-    
-	addScript: function(mode){
-		var script = document.createElement("script");
-		if(this.config.type == "minimal"){
-			script.innerHTML = "var download, upload;" +
-                "download = new JustGage({" +
-                "id: \"downloadSpeedGauge\"," +
-                "value: " + 0 + "," +
-                "min: 0," +
-                "max: " +  this.config.maxGaugeScale + "," +
-                "title: \"Download Speed\"," +
-                "refreshAnimationType:\"linear\"," +
-                "gaugeWidthScale: \"0.8\"," +
-                "valueFontColor: \"#fff\"," +
-                "valueFontFamily: \"Roboto Condensed\"," +
-                "titleFontFamily: \"Roboto Condensed\"," +
-                "titleFontColor: \"#aaa\"," +
-                "hideMinMax: true,"+
-                "gaugeColor: \"#000\","+
-                "levelColors: [\"#fff\"],"+
-                "hideInnerShadow: true,"+
-                "symbol: \"Mbps\"});" +
-                "upload = new JustGage({" +
-                "id: \"uploadSpeedGauge\"," +
-                "value: " + 0 + "," +
-                "min: 0," +
-                "max: " +  this.config.maxGaugeScale + "," +
-                "title: \"Upload Speed\"," +
-                "refreshAnimationType:\"linear\"," +
-                "gaugeWidthScale: \"0.8\"," +
-                "valueFontColor: \"#fff\"," +
-                "valueFontFamily: \"Roboto Condensed\"," +
-                "titleFontFamily: \"Roboto Condensed\"," +
-                "titleFontColor: \"#aaa\"," +
-                "hideMinMax: true,"+
-                "gaugeColor: \"#000\","+
-                "levelColors: [\"#fff\"],"+
-                "hideInnerShadow: true,"+
-                "symbol: \"Mbps\"});";
-
-		}
-		else{
-			script.innerHTML = "var download, upload;" +
-            "download = new JustGage({" +
-            "id: \"downloadSpeedGauge\"," +
-            "value: " + 0 + "," +
-            "min: 0," +
-            "max: " +  this.config.maxGaugeScale + "," +
-            "title: \"Download Speed\"," +
-            "refreshAnimationType:\"linear\"," +
-            "gaugeWidthScale: \"0.8\"," +
-            "valueFontColor: \"#fff\"," +
-            "valueFontFamily: \"Roboto Condensed\"," +
-            "titleFontFamily: \"Roboto Condensed\"," +
-            "titleFontColor: \"#aaa\"," +
-            "symbol: \"Mbps\"});" +
-            "upload = new JustGage({" +
-            "id: \"uploadSpeedGauge\"," +
-            "value: " + 0 + "," +
-            "min: 0," +
-            "max: " +  this.config.maxGaugeScale + "," +
-            "title: \"Upload Speed\"," +
-            "refreshAnimationType:\"linear\"," +
-            "gaugeWidthScale: \"0.8\"," +
-            "valueFontColor: \"#fff\"," +
-            "valueFontFamily: \"Roboto Condensed\"," +
-            "titleFontFamily: \"Roboto Condensed\"," +
-            "titleFontColor: \"#aaa\"," +
-            "symbol: \"Mbps\"});";
-		}
-		$(script).appendTo("body");
-
 	},
 
 	socketNotificationReceived: function(notification, payload) {
 
-
 		if (notification == "downloadSpeedProgress") {
 			if (this.config.displaySpeed) {
 				if (this.downloadBarState == "not_started") {
-					this.addScript();
 					this.downloadBarState = "started";
 				}
 				console.log("updating DOWNLOAD");
-				download.refresh(payload, this.config.maxGaugeScale);
+				this.download.refresh(payload, this.config.maxGaugeScale);
 			}
 
 		}
 		if (notification == "uploadSpeedProgress") {
 			if (this.config.displaySpeed) {
 				console.log("updating UPLOAD");
-				upload.refresh(payload, this.config.maxGaugeScale);
+				this.upload.refresh(payload, this.config.maxGaugeScale);
 			}
 		}
 		if (notification == "data") {
@@ -150,21 +83,24 @@ Module.register("internet-monitor",{
                     "  Distance:   " + payload.server.distance + " km").append("</div>");
 			}
 		}
+
 		if (notification == "ping") {
-			console.log("ping")
-			if (this.downloadBarState == "not_started")
+			console.log("ping");
+			if (this.downloadBarState == "not_started") {
 				this.updateDom();
+			}
+
 			if (this.config.displayStrength) {
-				var d = document.createElement("div");
-				if (this.downloadBarState == "not_started") {
-					d = document.createElement("div");
-					$(d).appendTo("#pingDiv");
-				}
-				else {
-					$("#pingDiv").children().remove();
-					d = document.createElement("div");
-					$(d).appendTo("#pingDiv");
-				}
+				// var d = document.createElement("div");
+				// if (this.downloadBarState == "not_started") {
+				// 	// d = document.createElement("div");
+				// 	$(d).appendTo("#pingDiv");
+				// }
+				// else {
+				// 	$("#pingDiv").children().remove();
+				// 	d = document.createElement("div");
+				// 	$(d).appendTo("#pingDiv");
+				// }
 
 				payload = 150;
 
@@ -181,7 +117,6 @@ Module.register("internet-monitor",{
 					else if (payload >= 150) {
 						$(d).append("<div class=\"wifi-symbol-1\" > <div class=\"wifi-circle first\"></div> <div class=\"wifi-circle second\"></div> <div class=\"wifi-circle third\"></div> <div class=\"wifi-circle fourth\"></div> </div>");
 					}
-
 
 					$(".wifi-symbol-1 .wifi-circle").css({
 						"border-color": this.config.wifiSymbol.fullColor,
@@ -229,7 +164,7 @@ Module.register("internet-monitor",{
 				}
 				else{
 					if(payload < 70){
-						$(d).append("<img src=\"" + this.data.path + "images/" + "strength_full.png\" width=" +  this.config.strengthIconSize +"\"height=" + this.config.strengthIconSize +"/>");
+						$(d).append("<img src=\"" + this.data.path + "images/" + "strength_full.png\" width=" + this.config.strengthIconSize +"\"height=" + this.config.strengthIconSize +"/>");
 					}
 					else if(payload >= 70 && payload < 100){
 						$(d).append("<img src=\"" + this.data.path + "images/" + "strength_almost.png\" width=" + this.config.strengthIconSize +"\"height=" + this.config.strengthIconSize + "/>");
@@ -246,37 +181,100 @@ Module.register("internet-monitor",{
 			}
 		}
 
-
 	},
 
 	getDom: function(){
 		var wrapper = document.createElement("div");
 		wrapper.className = "small";
-		console.log(this.config);
+		// console.log(this.config);
+
 		if(this.config.displayStrength)
 		{
 			console.log("creating pingDiv");
 			var pingDiv = document.createElement("div");
 			pingDiv.id = "pingDiv";
+			pingDiv.className = "strength";
+			pingDiv.innerHTML = "<b> testing </b> ";
 			wrapper.appendChild(pingDiv);
 		}
+		
 		if(this.config.displaySpeed) {
 			console.log("creating speed");
+			var minimal = ((this.config.type == "minimal") ? true : false);
+			
 			var downloadSpeedGauge = document.createElement("div");
 			downloadSpeedGauge.id = "downloadSpeedGauge";
+			downloadSpeedGauge.className = "speedGauge";
 
 			var uploadSpeedGauge = document.createElement("div");
 			uploadSpeedGauge.id = "uploadSpeedGauge";
+			uploadSpeedGauge.className = "speedGauge";
+
+			this.download = new JustGage({
+				// id: "downloadSpeedGauge",
+				parentNode: downloadSpeedGauge,
+				value: 0,
+				min: 0,
+				max: this.config.maxGaugeScale,
+				title: "Download Speed",
+				refreshAnimationType: "linear",
+				gaugeWidthScale: 0.8,
+				valueFontColor: "#FFFFFF",
+				valueFontFamily: "Roboto Condensed",
+				titleFontFamily: "Roboto Condensed",
+				titleFontColor: "#AAAAAA",
+				hideMindMax: minimal,
+				gaugeColor: "#000000",
+				levelColors: ["#FF0000","#FFFF00","#00FF00"],
+				hideInnerShadow: true,
+				symbol: "Mbps"
+			});
+
+			this.upload = new JustGage({
+				// id: "uploadSpeedGauge",
+				parentNode: uploadSpeedGauge,
+				value: 0,
+				min: 0,
+				max: this.config.maxGaugeScale,
+				title: "Upload Speed",
+				refreshAnimationType: "linear",
+				gaugeWidthScale: 0.8,
+				valueFontColor: "#FFFFFF",
+				valueFontFamily: "Roboto Condensed",
+				titleFontFamily: "Roboto Condensed",
+				titleFontColor: "#AAAAAA",
+				hideMindMax: minimal,
+				gaugeColor: "#000000",
+				levelColors: ["#FF0000","#FFFF00","#00FF00"],
+				hideInnerShadow: true,
+				symbol: "Mbps"
+			});
+
+			if (minimal) {
+				var downOpt = {
+					gaugeColor: "#000",
+					levelColors: "#fff",
+					hideInnerShadow: true,
+				};
+				var upOpt = {
+					gaugeColor: "#000",
+					levelColors: "#fff",
+					hideInnerShadow: true,
+				};
+				this.download.update(downOpt);
+				this.upload.update(upOpt);
+			}
 			wrapper.appendChild(downloadSpeedGauge);
 			wrapper.appendChild(uploadSpeedGauge);
 		}
+		
 		if(this.config.verbose) {
 			var data = document.createElement("div");
 			data.id = "internetData";
 			wrapper.appendChild(data);
 		}
+		
 		return wrapper;
 	}
-
 
 });
